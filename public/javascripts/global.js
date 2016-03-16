83cc4982ce085b7016963006bd99ece44f2bdadb
Rich <blueguitarenator@gmail.com>
2016-03-15 19:52:44 -0500

    initial commit

---
+++ public/javascripts/global.js
@@ -0,0 +1,236 @@
+﻿// Userlist data array for filling in info box
+var athlete = null;
+var sessions = [];
+
+// DOM Ready =============================================================
+$(document).ready(function () {
+    
+    // Search Email button click
+    $('#btnSearchEmail').on('click', searchEmail);
+    
+    // Choose a session
+    $('#btnChooseSession').on('click', chooseSession);
+    
+    $("#datepick").datepicker({ dateFormat: 'yy-mm-dd' });
+    
+    hideDashboard('userList');
+    hideDashboard('addUser');
+});
+
+// Functions =============================================================
+
+function showDashboard(part) {
+    var page = document.getElementById(part);
+    page.style.visibility = 'visible';
+    page.style.display = 'block';
+}
+
+function hideDashboard(part) {
+    var page = document.getElementById(part);
+    page.style.visibility = 'hidden';
+    page.style.display = 'none';
+}
+
+function chooseSession(event) {
+    event.preventDefault();
+    var date = $('#addUser input#datepick').val();
+    var hour = $('#addUser input#sessionHour:checked').val();
+    var data = {};
+    data.date = date;
+    data.hour = hour;
+    data.athleteId = athlete.id;
+    JSON.stringify(data);
+    // Use AJAX to post the session date and hour
+    $.ajax({
+        type: 'POST',
+        data: data,
+        url: '/users/athletesession',
+        dataType: 'JSON'
+    }).done(function (response) {
+        // Check for successful (blank) response
+        if (response.msg === '') {
+            // Update the view
+            populateDashboard();
+        }
+        else {
+            // If something goes wrong, alert the error message that our service returned
+            alert('Error: ' + response.msg);
+        }
+    });
+};
+
+// Fill table with data
+function populateDashboard() {
+    
+    $('#athleteName').text(athlete.fullname);
+    
+    // Empty content string
+    var tableContent = '';
+    
+    var url = '/users/sessions/' + athlete.id;
+    // jQuery AJAX call for JSON
+    $.getJSON(url, function (data) {
+        
+        // For each item in our JSON, add a table row and cells to the content string
+        $.each(data, function () {
+            tableContent += '<tr>';
+            tableContent += '<td>' + this.hour + '</td>';
+            tableContent += '<td>' + this.datetime + '</td>';
+            tableContent += '</tr>';
+        });
+        
+        // Inject the whole content string into our existing HTML table
+        $('#userList table tbody').html(tableContent);
+    });
+    showDashboard('userList');
+    showDashboard('addUser');
+};
+
+// Search Email
+function searchEmail(event) {
+    event.preventDefault();
+    var emailVal = $('#userInfo input#inputUserEmail').val();
+    if (emailVal != '') {
+        var data = {};
+        data.email = emailVal;
+        JSON.stringify(data);
+        // Use AJAX to post the email to our search service
+        $.ajax({
+            type: 'POST',
+            data: data,
+            url: '/users/search',
+            dataType: 'JSON'
+        }).done(function (response) {
+            // Check for successful (non-blank) response
+            if (response.length === 1) {
+                athlete = response[0];
+                console.log(athlete.fullname);
+                populateDashboard();
+            }
+            else {
+                // If something goes wrong, alert the error message that our service returned
+                alert('Unable to find user: ' + emailVal);
+                hideDashboard('userList');
+                hideDashboard('addUser');
+            }
+        });
+    }
+};
+
+// Show User Info - NOT USED
+function showUserInfo(event) {
+    
+    // Prevent Link from Firing
+    event.preventDefault();
+    
+    // Retrieve username from link rel attribute
+    var thisFullName = $(this).attr('rel');
+    
+    // Get Index of object based on id value
+    var arrayPosition = userListData.map(function (arrayItem) { return arrayItem.fullname; }).indexOf(thisFullName);
+    
+    // Get our User Object
+    var thisUserObject = userListData[arrayPosition];
+    
+    //Populate Info Box
+    $('#userInfoName').text(thisUserObject.fullname);
+    $('#userInfoAge').text(thisUserObject.age);
+    $('#userInfoGender').text(thisUserObject.gender);
+    $('#userInfoPhone').text(thisUserObject.phone);
+
+};
+
+// Add User - NOT USED REMOVE
+function addUser(event) {
+    event.preventDefault();
+    
+    // Super basic validation - increase errorCount variable if any fields are blank
+    var errorCount = 0;
+    $('#addUser input').each(function (index, val) {
+        if ($(this).val() === '') { errorCount++; }
+    });
+    
+    // Check and make sure errorCount's still at zero
+    if (errorCount === 0) {
+        
+        // If it is, compile all user info into one object
+        var newUser = {
+            'email': $('#addUser fieldset input#inputUserEmail').val(),
+            'fullname': $('#addUser fieldset input#inputUserFullname').val(),
+            'dob': $('#addUser fieldset input#inputUserDob').val(),
+            'age': $('#addUser fieldset input#inputUserAge').val(),
+            'gender': $('#addUser fieldset input#inputUserGender').val(),
+            'phone': $('#addUser fieldset input#inputUserPhone').val()
+        }
+        
+        // Use AJAX to post the object to our adduser service
+        $.ajax({
+            type: 'POST',
+            data: newUser,
+            url: '/users/adduser',
+            dataType: 'JSON'
+        }).done(function (response) {
+            
+            // Check for successful (blank) response
+            if (response.msg === '') {
+                
+                // Clear the form inputs
+                $('#addUser fieldset input').val('');
+                
+                // Update the table
+                populateTable();
+
+            }
+            else {
+                
+                // If something goes wrong, alert the error message that our service returned
+                alert('Error: ' + response.msg);
+
+            }
+        });
+    }
+    else {
+        // If errorCount is more than 0, error out
+        alert('Please fill in all fields');
+        return false;
+    }
+};
+
+// Delete User
+function deleteUser(event) {
+    
+    event.preventDefault();
+    
+    // Pop up a confirmation dialog
+    var confirmation = confirm('Are you sure you want to delete this user?');
+    
+    // Check and make sure the user confirmed
+    if (confirmation === true) {
+        
+        // If they did, do our delete
+        $.ajax({
+            type: 'DELETE',
+            url: '/users/deleteuser/' + $(this).attr('rel')
+        }).done(function (response) {
+            
+            // Check for a successful (blank) response
+            if (response.msg === '') {
+            }
+            else {
+                alert('Error: ' + response.msg);
+            }
+            
+            // Update the table
+            populateTable();
+
+        });
+
+    }
+    else {
+        
+        // If they said no to the confirm, do nothing
+        return false;
+
+    }
+
+};
---
+++ routes/users.js
@@ -0,0 +1,153 @@
+﻿var express = require('express');
+var router = express.Router();
+
+
+/*
+ * POST search athlete with email
+ */
+router.post('/search', function (req, res) {
+    var db = req.db;
+    console.log(req.body.email);
+    var strQuery = 'SELECT * FROM Athletes where email="' + req.body.email + '"';
+    console.log(strQuery);
+    var query = db.query(strQuery, function (err, rows) {
+        if (err) {
+            throw err;
+        } else {
+            console.log(rows);
+            res.json(rows);
+        }
+    });
+    console.log(query.sql);
+});
+
+/*
+ * GET athlete's sessions
+ */
+router.get('/sessions/:id', function (req, res) {
+    console.log("YAHOOOOOO");
+    var db = req.db;
+    var athleteId = req.params.id;
+    var strQuery = 'SELECT datetime, hour FROM Sessions s join AthleteSession ss ON s.id=ss.sessionId JOIN Athletes a ON ss.athleteId=a.id WHERE a.id=' + athleteId;
+    db.query(strQuery, function (err, rows) {
+        if (err) {
+            throw err;
+        } else {
+            console.log(rows);
+            res.json(rows);
+        }
+    });
+});
+
+router.post('/athletesession', function (req, res) {
+    var db = req.db;
+    var sessionDate = req.body.date;
+    var sessionHour = req.body.hour;
+    var athleteId = req.body.athleteId;
+    var sessionId = -1;
+    db.query('select * from Sessions where datetime between ? and ? and hour=?', [sessionDate, sessionDate, sessionHour], function (err, rows) {
+        if (err) throw err;
+        console.log("No error getting session");
+        if (rows.length === 0) {
+            // session does not exist yet.  Create it.
+            console.log("Session did not exist yet");
+            db.query('insert into Sessions (datetime, hour) values (?, ?)', [sessionDate, sessionHour], function (err, result) {
+                if (err) throw err;
+                console.log("No error inserting the new session");
+                sessionId = getSessionId(db, sessionDate, sessionHour);
+                console.log(sessionId);
+            });
+        } else {
+            // session already existed
+            console.log("This session already existed");
+            sessionId = rows[0].id;
+        }
+        console.log("ath: " + athleteId);
+        console.log("ses: " + sessionId);
+        db.query('INSERT INTO AthleteSession (athleteId, sessionId) VALUES (?, ?)', [athleteId, sessionId], function (err, result) {
+            if (err) {
+                console.log("There was an error inserting athleteSession");
+            } else {
+                console.log("No error insert AS");
+            }
+            res.send(
+                (err === null) ? { msg: '' } : { msg: err }
+            );
+
+        });
+    });
+});
+
+//var insertAthlete = function insertAthleteSession(req, res) {
+//    var db = req.db;
+//    db.query('INSERT INTO AthleteSession (athleteId, sessionId) VALUES (?, ?)', [req.body.athleteId, req.sessionId], function (err, result) {
+//        if (err) {
+//            throw err;
+//        }
+//        res.send(
+//            (err === null) ? { msg: '' } : { msg: err }
+//        );
+//    });
+//}
+//
+//var getSession = function getSession(req, res, next) {
+//    var db = req.db;
+//    console.log("SHIT: " + date + " " + hour + " " + id);
+//    db.query('select * from Sessions where datetime between ? and ? and hour=?', [sessionDate, sessionDate, sessionHour], function (err, rows) {
+//        console.log("Dinner");
+//        if (err) {
+//            console.log("select session err");
+//            throw err;
+//        }
+//        else {
+//            console.log("Did not error");
+//            if (rows.length === 0) {
+//                console.log("No session found. insert new session");
+//                db.query('insert into Sessions (datetime, hour) values (?, ?)', [sessionDate, sessionHour], function (err, result) {
+//                    if (err) throw err;
+//                    req.sessionId = result.id;
+//                });
+//            } else {
+//                console.log("Session exists. Callback with row: " + rows[0].id);
+//                req.sessionId = rows[0].id;
+//            }
+//        }
+//    });
+//    next();
+//}
+/*
+ * POST athlete's session choice
+ */
+//router.post('/athletesession', [getSession, insertAthlete]);
+// function(req, res, next) {
+//    var db = req.db;
+//    var sessionDate = req.body.date;
+//    var sessionHour = req.body.hour;
+//    var athleteId = req.body.athleteId;
+//    var sessionId = -1;
+//
+//    getSession(db, sessionDate, sessionHour, athleteId, function(err, sessionId) {
+//        if (err) {
+//            // error handling code goes here
+//            console.log("ERROR : ",err);
+//        } else {
+//            // code to execute on data retrieval
+//            console.log("result from db is : ",sessionId);
+//            insertAthleteSession(db, athleteId, sessionId, function(err, data) {
+//                res.send(
+//                    (err === null) ? { msg: '' } : { msg: err }
+//                );
+//            });
+//        }
+//    });
+//});
+
+
+
+function getSessionId(db, date, hour) {
+    db.query('select * from Sessions where datetime between ? and ? and hour=?', [date, date, hour], function(err, rows) {
+        return rows[0].id;
+    });
+};
+
+module.exports = router;
