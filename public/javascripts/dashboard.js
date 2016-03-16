e0ed60172a97deae10ce6ccb3476082580511f95
Rich <blueguitarenator@gmail.com>
2016-03-16 11:53:18 -0500

    change date event

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -11,7 +11,11 @@ $(document).ready(function () {
     // Choose a session
     $('#btnChooseSession').on('click', chooseSession);
     
-    $("#datepick").datepicker({ dateFormat: 'yy-mm-dd' });
+    $('#datepick').datepicker({ dateFormat: 'yy-mm-dd' });
+
+    $("#datepick").datepicker().on("change", function () {
+        changeDate();
+    });
     
     hideDashboard('userList');
     hideDashboard('addUser');
@@ -29,6 +33,10 @@ function hideDashboard(part) {
     var page = document.getElementById(part);
     page.style.visibility = 'hidden';
     page.style.display = 'none';
+}
+
+function changeDate(newDate) {
+    alert("Date changed " + $('#addUser input#datepick').val());
 }
 
 function chooseSession(event) {
@@ -115,122 +123,4 @@ function searchEmail(event) {
             }
         });
     }
-};
-
-// Show User Info - NOT USED
-function showUserInfo(event) {
-    
-    // Prevent Link from Firing
-    event.preventDefault();
-    
-    // Retrieve username from link rel attribute
-    var thisFullName = $(this).attr('rel');
-    
-    // Get Index of object based on id value
-    var arrayPosition = userListData.map(function (arrayItem) { return arrayItem.fullname; }).indexOf(thisFullName);
-    
-    // Get our User Object
-    var thisUserObject = userListData[arrayPosition];
-    
-    //Populate Info Box
-    $('#userInfoName').text(thisUserObject.fullname);
-    $('#userInfoAge').text(thisUserObject.age);
-    $('#userInfoGender').text(thisUserObject.gender);
-    $('#userInfoPhone').text(thisUserObject.phone);
-
-};
-
-// Add User - NOT USED REMOVE
-function addUser(event) {
-    event.preventDefault();
-    
-    // Super basic validation - increase errorCount variable if any fields are blank
-    var errorCount = 0;
-    $('#addUser input').each(function (index, val) {
-        if ($(this).val() === '') { errorCount++; }
-    });
-    
-    // Check and make sure errorCount's still at zero
-    if (errorCount === 0) {
-        
-        // If it is, compile all user info into one object
-        var newUser = {
-            'email': $('#addUser fieldset input#inputUserEmail').val(),
-            'fullname': $('#addUser fieldset input#inputUserFullname').val(),
-            'dob': $('#addUser fieldset input#inputUserDob').val(),
-            'age': $('#addUser fieldset input#inputUserAge').val(),
-            'gender': $('#addUser fieldset input#inputUserGender').val(),
-            'phone': $('#addUser fieldset input#inputUserPhone').val()
-        }
-        
-        // Use AJAX to post the object to our adduser service
-        $.ajax({
-            type: 'POST',
-            data: newUser,
-            url: '/users/adduser',
-            dataType: 'JSON'
-        }).done(function (response) {
-            
-            // Check for successful (blank) response
-            if (response.msg === '') {
-                
-                // Clear the form inputs
-                $('#addUser fieldset input').val('');
-                
-                // Update the table
-                populateTable();
-
-            }
-            else {
-                
-                // If something goes wrong, alert the error message that our service returned
-                alert('Error: ' + response.msg);
-
-            }
-        });
-    }
-    else {
-        // If errorCount is more than 0, error out
-        alert('Please fill in all fields');
-        return false;
-    }
-};
-
-// Delete User
-function deleteUser(event) {
-    
-    event.preventDefault();
-    
-    // Pop up a confirmation dialog
-    var confirmation = confirm('Are you sure you want to delete this user?');
-    
-    // Check and make sure the user confirmed
-    if (confirmation === true) {
-        
-        // If they did, do our delete
-        $.ajax({
-            type: 'DELETE',
-            url: '/users/deleteuser/' + $(this).attr('rel')
-        }).done(function (response) {
-            
-            // Check for a successful (blank) response
-            if (response.msg === '') {
-            }
-            else {
-                alert('Error: ' + response.msg);
-            }
-            
-            // Update the table
-            populateTable();
-
-        });
-
-    }
-    else {
-        
-        // If they said no to the confirm, do nothing
-        return false;
-
-    }
-
 };
